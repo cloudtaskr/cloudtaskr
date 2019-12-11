3379e15a91d17ba2b2548348a5195df76b805507
Julian Phillips <julian.p@me.com>
2019-12-11 17:06:31 -0500

    feat: Updated local backend settings to match project cloud settings, fixed naming issues till becoming able to connect to the database.

--- app.js
+++ app.js
@@ -1,78 +1,86 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 // additional middleware imports
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport')
-require('./config/passport')
-
-
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+require("./configs/passport");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // add cors to alow front end to make request to the back-end without being block by settings restrictions
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"] // <== this will be the URL of our React app (it will be running on port 3000)
+  })
+);
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Add Session
-app.use(session({
-  secret: "SuperSecretPasswordPhrase",
-  resave: true,
-  saveUninitialized: true,
-}))
+app.use(
+  session({
+    secret: "SuperSecretPasswordPhrase",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 // allow our app to use sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Express View engine setup
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
+
 // not sure what this does, look like it tells the express app what to render
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
 // routes
-app.use('/', require('./routes/index'));
-app.use('/api', require('./routes/task'));
-app.use('/users/', require('./routes/auth-routes'))
+app.use("/", require("./routes/index"));
+app.use("/api", require("./routes/task"));
+app.use("/users/", require("./routes/auth-routes"));
 
 module.exports = app;

07aa3a99bf38cd201be89e772b5967e53c128690
avrahm <me@avrahm.com>
2019-12-14 12:08:51 -0500

    add task connected to use

--- routes/task-routes.js
+++ routes/task-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router  = express.Router();
+
 // import the model that will be used for the tasks collection
 const Task = require('../models/task-model');
 const User = require('../models/user-model');
+
 // GET route => get all the tasks in the collection
 router.get("/tasks", (req, res, next) => {
   Task.find()
@@ -25,12 +27,12 @@ router.get('/projects', (req, res, next) => {
 
 // POST route => to create a new task
 router.post('/tasks', (req, res, next)=>{
- 
+ console.log("-=-=-=-=-=-=-=-=-=-=-=-=-",req.body, req.session, req.user)
   Task.create({
     title: req.body.title,
     description: req.body.description,
     // subTasks: []
-    owner: req.session.currentUser._id
+    owner: req.user._id
   })
     .then(response => {
       res.json(response);

92115e1bc7f7af0bbdc63aa045b4826fb0b3f921
avrahm <me@avrahm.com>
2019-12-16 11:59:04 -0500

    find by user id on tasks complete

--- models/user-model.js
+++ models/user-model.js
@@ -3,8 +3,10 @@ const PLM = require('passport-local-mongoose');
 
 const userSchema = new Schema(
   {
-    email: String,
-    password: String
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
   },
   {
     timestamps: true,

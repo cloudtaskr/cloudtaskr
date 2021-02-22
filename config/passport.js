6a1ceb408bc13d64937228ce6eebcd6b72d3226f
Avrahm <avrahmk@me.com>
2021-02-21 22:21:08 -0500

    Updates

--- config/passport.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/user-model');
-const passport = require('passport');
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser()); 
-passport.deserializeUser(User.deserializeUser());
-
-module.exports = passport;
---
+++ precache-manifest.347102ca3e10f04914d360055d4ef19e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab2062706dd8861c4c6",
+    "url": "/static/css/main.7093a942.chunk.css"
+  },
+  {
+    "revision": "4ab2062706dd8861c4c6",
+    "url": "/static/js/main.e6f02068.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7f559c05b70ab49b9db9",
+    "url": "/static/css/2.a047c0ba.chunk.css"
+  },
+  {
+    "revision": "7f559c05b70ab49b9db9",
+    "url": "/static/js/2.206b502c.chunk.js"
+  },
+  {
+    "revision": "b652da20136db96db8c9521249894567",
+    "url": "/index.html"
+  }
+];

cbff4939bc1aa28cdb12f9c3018db0d79f1e1339
Avrahm <avrahmk@me.com>
2021-02-21 23:23:53 -0500

    toml file to handle cors

--- src/services/base.js
+++ src/services/base.js
@@ -2,7 +2,7 @@ let baseURL;
 
 process.env.NODE_ENV === "production"
   ? //? (baseURL = 'here should be your production endpoint')
-    (baseURL = "https://cloudtaskr-backend.netlify.app")
+    (baseURL = "https://cloudtaskr-backend.netlify.app/")
   : (baseURL = "http://localhost:5000");
 
   export default baseURL
